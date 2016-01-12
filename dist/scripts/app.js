e381f920701af54e8b75f87de49655e0ddffe6ba
Anthony Barba <abarba222@gmail.com>
2016-01-12 05:15:13 -0500

    entered all new files

---
+++ dist/scripts/app.js
@@ -0,0 +1,14 @@
+require.register("main", function(exports, require, module){
+  'use strict';
+
+(function () {
+  'use strict';
+
+  $(document).ready(function () {
+    $('.app').prepend(JST.index());
+  });
+})();
+  
+});
+
+//# sourceMappingURL=app.js.map
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    $('.app').prepend(JST.index());
+  });
+})();
