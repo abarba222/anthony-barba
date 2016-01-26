e4dd63aa5b6cc4d9f15851e44118a498eecd48fe
Anthony Barba <abarba222@gmail.com>
2016-01-26 16:26:16 -0500

    js, hbs, scss changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,11 @@
 
   $(document).ready(function(){
     $('.app').prepend(JST.index());
+
+    $('#drunken-project').on('click', function() {
+      window.open('http://abarba222.github.io/drunken_diplomacy/', 'Drunken Diplomacy', 'height=700px, width=1280px');
+    });
+
+    
   });
 })();
