29c18a2135ba373987ad45f40e94a6961686a1e6
Anthony Barba <abarba222@gmail.com>
2016-01-26 16:49:31 -0500

    hopefully fixed gh-pages

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -6,6 +6,10 @@ require.register("main", function(exports, require, module){
 
   $(document).ready(function () {
     $('.app').prepend(JST.index());
+
+    $('#drunken-project').on('click', function () {
+      window.open('http://abarba222.github.io/drunken_diplomacy/', 'Drunken Diplomacy', 'height=700px, width=1280px');
+    });
   });
 })();
   
